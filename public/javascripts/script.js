2061f2d1311af70a31e077ec6130f7c53c95f7b9
Craig Maughan <craigmaughan@gmail.com>
2017-12-13 01:39:57 +0000

    swagswagjosh

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,5 @@
 var userName;
 var userImg;
-
-console.log('loading');
-
-
-
 var myApp = angular.module("myApp", ["firebase"]);
 
 myApp.controller("chatController", ["$scope", "$firebaseArray",
@@ -22,14 +17,14 @@ myApp.controller("chatController", ["$scope", "$firebaseArray",
             $scope.chats.$add(newmessage);
             user.chat = "";
         }
-        
+
         firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        userName = user.displayName;
-        userImg = user.photoURL;
-        console.log(user.name + "signed in");
-        $scope.userName = userName;
-    }
-});
+            if (user) {
+                userName = user.displayName;
+                userImg = user.photoURL;
+                console.log(user.name + "signed in");
+                $scope.userName = userName;
+            }
+        });
  }
 ]);
